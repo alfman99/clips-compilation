fa16667fbc8fb93b8b82f58169e102e0093521b4
alfredo <alfredo@nestersoftware.com>
2024-05-11 00:25:43 +0200

    feat: update versions and create daily video

--- src/OutroSencilla.tsx
+++ src/OutroSencilla.tsx
@@ -4,17 +4,21 @@ const OutroSencilla = () => {
 
   return (
     <>
-    <Audio src={staticFile('outro_song.mp3')} volume={0.35} />
-    <div style={{ 
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      zIndex: -1,
-      backgroundColor: '#141517',
-      filter: "brightness(0.6)",
-    }}>
-      <Video loop muted src={staticFile('outro.mp4')} className="h-full w-full object-cover" style={{ filter: "blur(1.5em)" }} />
-    </div>
+      <h1 style={{
+        position: 'absolute',
+        top: '40%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        fontSize: '5em',
+        fontWeight: 'bold',
+        zIndex: 1000,
+      }}>Clips.fail</h1>
+      <div style={{
+        backgroundColor: 'black',
+      }}>
+        <Audio src={staticFile('outro_song.mp3')} volume={0.35} />
+        <Video width={'100%'} loop muted src={staticFile('outro.mp4')} className="h-full w-full object-cover" style={{ filter: "blur(1.5em)" }} />
+      </div>
     </>
   )
 }
